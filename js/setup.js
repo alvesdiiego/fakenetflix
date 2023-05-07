85062cb0f4022e9035386c8a8e64b5cd4083999b
Diego Alves <diegoalvesribeiro1993@gmail.com>
2023-05-07 14:50:51 -0300

    terceiro

--- js/setup.js
+++ js/setup.js
@@ -17,3 +17,12 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+function abrirMenu() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
